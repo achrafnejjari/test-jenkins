8904baf458b105a63523e9e764ca81accb4f126f
achrafnejjari <achrafnejjari49@gmail.com>
2025-11-26 20:34:51 +0100

    i add if else to the Test file

--- Test.java
+++ Test.java
@@ -5,6 +5,15 @@ public class Test{
         System.out.println("hello word");
         System.out.println("test java jenkins !");
 
+        int a=5;
+        double b=6.5;
+
+        if(a<b){
+            System.out.println("True !");
+        }else{
+            System.out.println("false !!!");
+        }
+
 
     }
 }
