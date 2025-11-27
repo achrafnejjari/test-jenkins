72fb3563e8deac53d528ef2a5344b74b7cd01c45
achrafnejjari <achrafnejjari49@gmail.com>
2025-11-27 17:32:44 +0100

    update

--- Test2.java
+++ Test2.java
@@ -3,7 +3,11 @@ public class Test2 {
     public static void main(String[] args) {
         for(int i=0 ; i<4 ; i++){
         System.out.println("achraf nj !!");
+
+        System.out.println("hi");
     }
+
+
     }
     
 }
